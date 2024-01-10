87e19c46895f2b794e8e8f302fa1018e6a42e6b2
redp <redplait@gmail.com>
2024-01-10 18:13:59 +0300

    est_adj function

--- Add/hs1.cc
+++ Add/hs1.cc
@@ -25,9 +25,8 @@ inline void printTime(const char *pfx)
 struct item
 {
   int64_t v;
-  int idx;
   int c_idx = -1; // -1 means that element is not in cluster
-  int left, size, centr_idx;
+  int left, size, centr_idx, flipped = -1;
 };
 
 void find_centroid(vector<item> &h, int idx)
@@ -47,6 +46,44 @@ void find_centroid(vector<item> &h, int idx)
   for ( int i = 0; i < h[idx].size; i++ ) h[idx+i].centr_idx = new_c;
 }
 
+void debug_dump(const vector<item> &h)
+{
+  for ( int i = 0; i < h.size(); i++ )
+  {
+    printf("[%d] %ld", i, h[i].v);
+    if ( h[i].c_idx == -1 )
+    {
+      printf("\n");
+      continue;
+    }
+    printf(" c_idx %d left %d size %d centr %d\n", h[i].c_idx, h[i].left, h[i].size, h[i].centr_idx);
+  }
+}
+
+int64_t est_adj(const vector<item> &h, int idx, int &new_c)
+{
+  // find start of left cluster
+  int left = idx;
+  for ( int i = idx; i >= 0; i-- )
+    if ( h[i].c_idx != h[idx].c_idx ) break;
+    else left = i;
+  // calc new centroid
+  double sum = 0.0, prod = 0.0;
+  for ( int i = 0; i < h[left].size + h[idx+1].size; i++ )
+  {
+    sum += h[left+i].v;
+    if (i) prod += h[left+i].v * i;
+  }
+  new_c = round(prod / sum) + left;
+  int64_t res = 0;
+  for ( int i = 0; i < h[left].size + h[idx+1].size; i++ )
+  {
+    if ( i + left == new_c ) continue;
+    res += abs(i + left - new_c) * h[i + left].v;
+  }
+  return res;
+}
+
 int main()
 {
   int n, k;
@@ -56,56 +93,67 @@ int main()
     puts("0"); return 0;
   }
   vector<item> h(n);
-  for ( int i = 0; i < n; i++ )
-  {
-    h[i].idx = i;
-    cin>>h[i].v;
-  }
+  for ( int i = 0; i < n; i++ ) cin>>h[i].v;
   int c_idx = 0;
   int curr_k = n;
   for( ; curr_k != k; curr_k-- )
   {
-    int64_t best = LONG_MAX;
-    int best_i = -1;
+    int64_t v, best = LONG_MAX;
+    int best_i = -1, curr_c, new_c;
+    bool cut_l = false, cut_r = false;
     for ( int i = 0; i < n - 1; i++ )
     {
       // we have 4 cases - when both current and right not in cluster
       if ( -1 == h[i].c_idx && -1 == h[i+1].c_idx )
       {
-        int64_t v = min(h[i].v, h[i+1].v);
-        if ( v < best ) { best = v; best_i = i; }
+        v = min(h[i].v, h[i+1].v);
+        if ( v < best ) { best = v; best_i = i; cut_l = cut_r = false; }
         continue;
       }
       // 2) left not in cluster and right in cluster
       if ( -1 == h[i].c_idx && -1 != h[i+1].c_idx )
       {
-        int64_t v = abs(i - h[i+1].centr_idx) * min(h[i].v, h[h[i+1].centr_idx].v);
-        if ( v < best ) { best = v; best_i = i; }
+        v = abs(i - h[i+1].centr_idx) * min(h[i].v, h[h[i+1].centr_idx].v);
+        if ( v < best ) { best = v; best_i = i; cut_l = cut_r = false; }
         continue;
       }
       // 3) left in cluster and right not
       if ( -1 != h[i].c_idx && -1 == h[i+1].c_idx )
       {
-        int64_t v = abs(i + 1 - h[i].centr_idx) * min(h[i+1].v, h[h[i].centr_idx].v);
-        if ( v < best ) { best = v; best_i = i; }
+        v = abs(i + 1 - h[i].centr_idx) * min(h[i+1].v, h[h[i].centr_idx].v);
+        if ( v < best ) { best = v; best_i = i; cut_l = cut_r = false; }
         continue;
       }
       // final case - both in clusters
       if ( h[i].c_idx == h[i+1].c_idx ) continue;
-      int64_t v = abs(h[i].centr_idx - h[i+1].centr_idx) * min(h[h[i].centr_idx].v, h[h[i+1].centr_idx].v);
-      if ( v < best ) { best = v; best_i = i; continue; }
-      // check if  node should be merged with right cluster
-//      v = abs(i - h[i+1].centr_idx) * h[i].v;
-//      if ( v < best ) { best = v; best_i = i; cut_l = true; continue; }
-      // check if node should be merged with left cluster
-//      if ( i )
-//      {
-//        v = abs(i - h[i-1].centr_idx) * h[i].v;
-//        if ( v < best ) { best = v; best_i = i; cut_r = true; continue; }
-//      }
+    //  v = abs(h[i].centr_idx - h[i+1].centr_idx) * min(h[h[i].centr_idx].v, h[h[i+1].centr_idx].v);
+      // check if node[i] should be merged with right cluster
+      if ( h[i].flipped != h[i+1].c_idx )
+      {
+        v = abs(i - h[i+1].centr_idx) * h[i].v;
+        if ( v < best ) { best = v; best_i = i; cut_l = true; cut_r = false; }
+      }
+      // check if node[i+1] should be merged with left cluster
+      if ( h[i+1].flipped != h[i].c_idx )
+      {
+        v = abs(i + 1 - h[i].centr_idx) * h[i+1].v;
+        if ( v < best ) { best = v; best_i = i; cut_r = true; cut_l = false; }
+      }
+      // finally check if we need to merge 2 adjacent cluster
+      v = est_adj(h, i, curr_c);
+      if ( v < best )
+      {
+        best = v;
+        new_c = curr_c;
+        best_i = i;
+        cut_l = cut_r = false;
+      }
     }
 #ifdef DEBUG
- printf("best_i %d, l_idx %d r_idx %d, best %ld\n", best_i, h[best_i].c_idx, h[best_i+1].c_idx, best);
+ printf("best_i %d, l_idx %d r_idx %d, best %ld", best_i, h[best_i].c_idx, h[best_i+1].c_idx, best);
+ if ( cut_l ) printf(" cut_l");
+ if ( cut_r ) printf(" cut_r");
+ printf("\n");
 #endif
     // merge best_i & best_i + 1 into cluster
     // we again have 4 cases here
@@ -147,10 +195,11 @@ int main()
     for ( int i = best_i; i >= 0; i-- )
       if ( h[i].c_idx != h[best_i].c_idx ) break;
       else left = i;
-/*    if ( cut_l )
+    if ( cut_l )
     {
       h[left].size--;
       if ( h[left].size < 2 ) { h[left].c_idx = -1; }
+      else h[best_i].flipped = h[best_i].c_idx;
       h[best_i].size = h[best_i+1].size + 1;
       h[best_i].c_idx = h[best_i+1].c_idx;
       h[best_i].left = best_i;
@@ -159,13 +208,32 @@ int main()
       find_centroid(h, best_i);
       curr_k++;
       continue;
-    } */
+    }
+    // connect node[best_i + 1] with cluster at left
+    if ( cut_r )
+    {
+      h[best_i+2].size = h[best_i+1].size - 1;
+      if ( h[best_i+2].size < 2 ) { h[best_i+2].c_idx = -1; }
+      else h[best_i+2].left = best_i + 2;
+      h[best_i+1].flipped = h[best_i+1].c_idx;
+      h[left].size++;
+      h[best_i+1].c_idx = h[left].c_idx;
+      if ( h[best_i+2].c_idx != -1 )
+        find_centroid(h, best_i+2);
+      find_centroid(h, left);
+      curr_k++;
+      continue;
+    }
+    // merge 2 adjacent cluster
     for ( int i = 0; i < h[best_i+1].size; i++ )
     {
       h[best_i+1+i].c_idx = h[left].c_idx;
       h[left].size++;
     }
-    find_centroid(h, left);
+#ifdef DEBUG
+ printf("new centr_idx %d\n", new_c);
+#endif
+    for ( int i = 0; i < h[left].size; i++ ) h[i+left].centr_idx = new_c;
   }
   // dump
   int64_t diff = 0;
