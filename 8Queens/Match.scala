bf1c4b9725edba29a59bb65e34e43fd19b64213e
redp <redplait@gmail.com>
2024-12-13 11:08:39 +0300

    matches trash

---
+++ 8Queens/Match.scala
@@ -0,0 +1,20 @@
+object Match {
+
+// from https://www.youtube.com/watch?v=dQ8ctYg3y8U
+// each next level adds 4 for right-upper square +
+// 2 matches * n - 1 for upper row +
+// 2 matches * n - 1 for rigth column - so
+// 2 * 2 * (n-1)
+
+def calc( n: Int ): Int = n match
+ case 1 => 4
+ case _ => 4 + 4 * (n - 1) + calc( n - 1 )
+
+def main(args: Array[String]): Unit = {
+ val usage = """
+    Usage: match size
+  """
+ if (args.length == 0) println(usage)
+ else println( calc( args(0).toInt ) )
+}
+}
