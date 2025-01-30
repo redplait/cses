390d4193bd8f8989e6db9c1030bae929280422fc
redp <redplait@gmail.com>
2025-01-30 17:16:56 +0300

    takeWhile instead of break

--- Euler/p044.scala
+++ Euler/p044.scala
@@ -1,7 +1,6 @@
 import scala.util.boundary, boundary.break
 
 @main def calc() = {
- object AllDone extends Exception { }
  // idea stolen from https://martin-ueding.de/posts/project-euler-solution-44-pentagon-numbers/
  def isPent(i: Int): Boolean = {
    val s = Math.round(Math.sqrt(1 + 24 * i))
@@ -10,19 +9,14 @@ import scala.util.boundary, boundary.break
  // pentagonal numbers stream
  def pent(p: Int = 1, d: Int = 4): LazyList[Int] =
     LazyList.cons(p, pent(p + d, d + 3))
- try {
- for ( p_diff <- pent() ) boundary {
-   for ( pair <- pent().sliding(2).map{case Seq(x,y) => (x,y)} ) {
-    val upper = pair._1 + p_diff
-    if ( upper < pair._2 ) then break()
-    val sum = pair._1 + upper
-    if ( isPent(upper) && isPent(sum) ) {
+ boundary {
+ for ( p_diff <- pent();
+       pair <- pent().sliding(2).map( _ match {case Seq(x,y) => (x,y, x + p_diff)}).takeWhile( x => x._3 >= x._2 ) ) {
+    val sum = pair._1 + pair._3
+    if ( isPent(pair._3) && isPent(sum) ) {
       println(p_diff)
-      throw AllDone
+      break()
     }
-   }
   }
- } catch {
-   case AllDone =>
  }
 }
